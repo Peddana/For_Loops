bf3f1f74967806dff2b3125a08ef62103ffeaced
peddanna <peddanna310@gmail.com>
2023-07-29 18:22:04 +0530

    Commiting For loop

---
+++ For_Loop_Test_2.py
@@ -0,0 +1,7 @@
+languages = ['selenium', 'Python',  'Oracle', 'Mssql', 'Mysql']
+
+#for i in range(len(languages)):
+#   print('Current language:', languages[i])
+
+for i in range(len(languages)):
+   print (i, languages[i])
